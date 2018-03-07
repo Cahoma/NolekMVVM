78a2def36b0a57092ba349a8af6cef17c454258f
Casper <casp.mad@gmail.com>
2018-03-07 15:58:23 +0100

    Revert "Revert "Can create users""
    This reverts commit 0ad3fc99cf0e0fd4b3e20158a11b77b099b58da4.

---
+++ NolekWPF/Data/Repositories/UserRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NolekWPF.DataAccess;
+using NolekWPF.Model;
+using NolekWPF.Model.Dto;
+using NolekWPF.Wrappers;
+using System.Collections.ObjectModel;
+
+namespace NolekWPF.Data.Repositories
+{
+    public class UserRepository: IUserRepository
+    {
+        private wiki_nolek_dk_dbEntities _context;
+
+        public UserRepository(wiki_nolek_dk_dbEntities context)
+        {
+            _context = context; //context is kept alive throughout the application lifetime
+        }
+
+        public void Add(Login model)
+        {
+            _context.Logins.Add(model); //call insert to add new equipement to table
+        }
+
+        public bool HasChanges()
+        {
+            return _context.ChangeTracker.HasChanges(); //return true if current equipement has changes
+        }
+
+        public void Remove(Login model)
+        {
+            _context.Logins.Remove(model); //delete equipment from the db
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync(); //save all changes to the current context
+        }
+    }
+}
---
+++ NolekWPF/ViewModels/Main/IUserCreateViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using NolekWPF.Model;
+using NolekWPF.Wrappers;
+using System.Collections.ObjectModel;
+
+namespace NolekWPF.ViewModels.Main
+{
+    public interface IUserCreateViewModel
+    {
+        ICommand CreateUserCommand { get; }
+        Login NewUser { get; }
+
+        ObservableCollection<UserLookup> Users { get; }
+        Task LoadAsync();
+    }
+}
